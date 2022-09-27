0e900dfba03fc847e8f4497ad6504634a9d08edf
Ahmed Ummar <ac4636@student.jamk.fi>
2022-09-27 21:32:59 +0300

    basic app
    basic application is here

---
+++ BasicApp/BasicApp/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace BasicApp
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
---
+++ BasicApp/BasicApp/ViewController.designer.cs
@@ -0,0 +1,25 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace BasicApp
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Action ("pushButton:")]
+		partial void pushButton (AppKit.NSButton sender);
+
+		[Action ("resetButton:")]
+		partial void resetButton (AppKit.NSButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
